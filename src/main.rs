c15a363897d0c4a2e86cc149cd81ef200199916e
Adam Hines <adam.hines@qut.edu.au>
2026-06-10 16:37:08 +0200

    starting with rust

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("Hello, world!");
+    // stop right there, thank you very much
+    println!("Wowowowweeee waaaa!");
+    /* what in the worrrrrrld?
+    is happening to me?*/
+}
